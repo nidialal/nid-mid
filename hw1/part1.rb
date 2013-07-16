34ef33bcabfc798e86cef6f709af338837a1f625
nidialal <nidialal@gmail.com>
2013-07-16 19:49:17 +1200

    function definition done

---
+++ hw1/part1.rb
@@ -0,0 +1,29 @@
+#For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
+#file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
+#satisfied with your work, save your file and upload it to the checker.
+
+
+def palindrome?(str)
+  # YOUR CODE HERE
+  str1=str.downcase.gsub(/[^a-z]/i,'')
+  str2=str1.reverse
+  if(str1==str2)
+	return true
+  else
+	return false
+  end
+end
+
+def count_words(str)
+  arr=str.downcase.split(%r{\b})
+  arr.delete_if{|word| word==" "}
+  word_count={}
+  arr.each do|word|
+	if(word_count[word]==nil)
+	 word_count[word]=1
+	else
+	 word_count[word]=word_count[word]+1
+	end
+  end
+  return word_count
+end
